827b833618c7676b439b6ed157b7b27bdc01320c
AlissaPuumets <alissapuumets@gmail.com>
2024-11-10 03:49:17 +0200

    TÖÖTAB

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ todoForm.addEventListener('submit', function(e){
 })
 
 function addTodo(){
-    const todoText = todoInput.value.trim;
-    if(todoText.lenght > 0){
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0){
         allTodos.push(todoText)
         createTodoItem(todoText);
         updateTodoList();
@@ -30,7 +30,7 @@ function createTodoItem(todo, todoIndex){
     const todoId = "todo-"+todoIndex;
     const todoLI = document.createElement("li");
     todoLI.className = "todo";
-    todo.innerHTML = `
+    todoLI.innerHTML = `
     <input type="checkbox" id="${todoId}">
                 <label class="custom-checkbox" for="${todoId}">
                     <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
